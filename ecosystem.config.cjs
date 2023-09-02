8477d37fd206e71554ec68c387f592675ed6b025
Dago <tocarralero@gmail.com>
2023-09-02 01:32:57 -0400

    fix: deploy cmd

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "store.railab.tech",
-            script: "NODE_ENV=production node ./server.js",
+            script: "NODE_ENV=production pnpm remix-serve build",
         },
     ],
 };
